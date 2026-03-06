36742c21eeb45d26ca5edb2bfbc2158ed3de03c4
F1XON <slobodyanzora@mail.ru>
2026-03-07 03:05:09 +0500

    Обновил main

--- main_cpp/main.cpp
+++ main_cpp/main.cpp
@@ -5,9 +5,13 @@
 #include <filesystem>
 #include <windows.h>
 
-#include "aboba.cpp"
+#include "cliout.cpp"
 #include "searchNameFile.cpp"
 
 int main(){
-    std::cout << "aboba" << std::endl;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, "ru");
+
+    cliout::startProgram();
 }
