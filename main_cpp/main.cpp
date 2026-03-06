ed015c81d8f79dd05c15ff5d6036edd4e8d8f53e
F1XON <slobodyanzora@mail.ru>
2026-03-07 02:33:58 +0500

    Добавил начальный запрос на директорию проекта

--- main_cpp/main.cpp
+++ main_cpp/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <windows.h>
+
+#include "aboba.cpp"
+#include "searchNameFile.cpp"
 
 int main(){
-    std::cout << "aboba" << std::endl;
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, "ru");
+
 }
